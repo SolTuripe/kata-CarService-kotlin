5a49f474d7e480f98633878e5abfbc7cb6e22e8a
Sol Turipe <solcis.bio@gmail.com>
2022-10-30 20:44:48 +0100

    add Rent class

---
+++ src/main/kotlin/com/carservice/kata/model/Rent.kt
@@ -0,0 +1,11 @@
+package com.carservice.kata.model
+
+import java.util.*
+
+
+public class Rent(rentingId: Long, rentingUser: User, rentedCar: Car, date: Date) {
+    private var rentingId = rentingId
+    private var rentingUser = rentingUser
+    private var rentedCar = rentedCar
+    private var date = date
+}
